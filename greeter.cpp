3104f36b7bc53e892dbe8dbf62cc94ec7f06057f
karu-rress <nsun5274@gmail.com>
2024-10-09 13:45:41 +0900

    Greeter - Fin

--- greeter.cpp
+++ greeter.cpp
@@ -9,10 +9,9 @@
  */
 
 #include <iostream>
+#include <string_view>
 
 #include "greeter.h"
-
-using namespace std;
 
 Greeter::Greeter(PlanManager &pm, RecipeDatabase &rdb)
     : planManager(pm)
@@ -20,79 +19,98 @@ Greeter::Greeter(PlanManager &pm, RecipeDatabase &rdb)
 }
 
 void Greeter::run() {
-    printInitialMessage();
-    Option option = getUserOption();
+    while (true) {
+        printInitialMessage();
 
-    switch (option) {
-    case Option::BrowseRecipe:
-
-        recipeDatabase.searchRecipes();
-    case Option::AddRecipe:
-        recipeDatabase.addRecipe();
-        break;
-    case Option::EditRecipe:
-        recipeDatabase.editRecipe();
-        break;
-    case Option::ReviewPlan:
-        planManager.showPlans();
-        break;
-    case Option::CreatePlan:
-        planManager.createNewPlan();
-        break;
-    case Option::Quit:
-        exit(0);
+        switch (Option option = getUserOption(); option) {
+        case Option::BrowseRecipe:
+            recipeDatabase.searchRecipes();
+        case Option::AddRecipe:
+            recipeDatabase.addRecipe();
+            break;
+        case Option::EditRecipe:
+            recipeDatabase.editRecipe();
+            break;
+        case Option::ReviewPlan:
+            planManager.showPlans();
+            break;
+        case Option::CreatePlan:
+            planManager.createNewPlan();
+            break;
+        case Option::Quit:
+            exit(0);
+        }
     }
 }
 
 void Greeter::printInitialMessage() {
-    std::cout << "================ IIKH ================" << std::endl;
-    std::cout << "Welcome to IIKH, the Interactive Intelligent Kitchen Helper" << std::endl
-              << std::endl;
-    std::cout << "Press Return to begin" << std::endl
-              << std::endl;
+    std::string_view logo = R"(
+    ..                                                     
+   .= .+==:                                                
+   -      :-.:-::                                          
+  ..         .--  ..=:                                     
+  =              .-:    :--.                               
+    .+                +.     -:                            
+        :-.              :-.    :-:                        
+            -:.              =:.    .=.                    
+               .-:              .--    .:-                 
+                 .:+-.           :   .=:.. .=:.            
+            .=-:-:.:= .:=.      .=        .:-- :-.         
+      .:-+. -:..-: :-. -:..-:.  -               .=:=-.     
+  =-.= ..=..-- .=...=..-- .-: .+:                    ..+:  
+  = .-: --..-:..-: :-. --..-:.:-: .--::                .:  
+  =     .+. --  =: .+  --  =: .+. :=     :+.           -   
+  =        .-: .-: :-..--..-: :-:.--..-:      :--:    -.   
+  =             =...=. --. =...=. :=. =:..=.        -+:    
+  =                :-..:-..-- :-:.:- .:- .-: :-.--:.  :    
+  =                    --..=:..=. :-. =:..+=-.        :    
+  =                       .-- .-..:=.=-:.             :    
+  =.                          .=--                    :    
+    .-:                        :                      :    
+        :-.                    :                      :    
+           .--                 :                      :    
+               .-:             :                     -:    
+                   .+.         :              ..=+.        
+                       :-.     :          :-:.             
+                           =:. :   ..-=.                   
+                              .=-:.                        
+)";
+
+    std::cout << logo << "\n\n";
+    std::cout << "================ IIKH ================\n";
+    std::cout << "Welcome to IIKH, the Interactive Intelligent Kitchen Helper\n";
+    std::cout << "Press Return to begin\n";
+    std::cin.ignore();
+
     std::cout << "Please select an option:" << std::endl;
 
-    /**
-     * 1. Search recipes
-    -Casually browse the database of existing recipes, but without reference to
-    any particular meal plan.
-    2. Add a new recipe
-    -Add a new recipe to the database.
-    3. Edit recipe
-    -Edit or annotate an existing recipe.
-    4. See existing meal plans or See exsting plans of meals
-    -Review an existing plan for several meals.
-    5. Create a new plan of meals or Create a new meal plan
-    -Create a new plan of meals.
-     */
-
-    std::cout << "1. Search recipes" << std::endl;
-    std::cout << "2. Add a new recipe" << std::endl;
-    std::cout << "3. Edit a recipe" << std::endl;
-    std::cout << "4. Review meal plans" << std::endl;
-    std::cout << "5. Create a new meal plan" << std::endl;
-    std::cout << "6. Quit" << std::endl;
+    std::cout << "1. Search recipes\n"
+              << "2. Add a new recipe\n"
+              << "3. Edit a recipe\n"
+              << "4. Review meal plans\n"
+              << "5. Create a new meal plan\n"
+              << "6. Quit" << std::endl;
 }
 
 Option Greeter::getUserOption() {
     int option;
-    std::cin >> option;
 
-    switch (option) {
-    case 1:
-        return Option::BrowseRecipe;
-    case 2:
-        return Option::AddRecipe;
-    case 3:
-        return Option::EditRecipe;
-    case 4:
-        return Option::ReviewPlan;
-    case 5:
-        return Option::CreatePlan;
-    case 6:
-        return Option::Quit;
-    default:
-        std::cout << "Invalid option. Please try again." << std::endl;
-        return getUserOption();
+    while (true) {
+        switch (std::cin >> option; option) {
+        case 1:
+            return Option::BrowseRecipe;
+        case 2:
+            return Option::AddRecipe;
+        case 3:
+            return Option::EditRecipe;
+        case 4:
+            return Option::ReviewPlan;
+        case 5:
+            return Option::CreatePlan;
+        case 6:
+            return Option::Quit;
+        default:
+            std::cout << "Invalid option. Please try again." << std::endl;
+        }
     }
 }
