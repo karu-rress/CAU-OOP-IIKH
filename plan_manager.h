d017312c9b412d269e92ee380874b0a9fd605852
HUISMI <huisboy@gmail.com>
2024-10-11 03:44:42 +0900

    다중 포함 방지 매크로 위치 수정

--- plan_manager.h
+++ plan_manager.h
@@ -6,16 +6,15 @@
  * @brief: Plan의 데이터를 저장 및 호출하는 기능들
  *
  */
+#ifndef _PLAN_MANAGER_H_
+#define _PLAN_MANAGER_H_
 
 #include <filesystem>
 #include <fstream>
 #include <map>
 
-#include "meal.h"
 #include "date.h"
-
-#ifndef _PLAN_MANAGER_H_
-#define _PLAN_MANAGER_H_
+#include "meal.h"
 
 class PlanManager {
 public:
